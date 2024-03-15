d6f8a61e8a9f5335b537241d4ad41a54d5d0a5b6
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2024-03-15 16:52:58 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,6 @@
-const menu = document.getElementById("menu")
-const menu = document.getElementById("cart-btn")
+const menu = document.getElementById("menu") //footer//
+const cartBtn = document.getElementById("cart-btn") //adicionar no carrinho//
+const cartModal = document.getElementById("cart-modal") // modal
+const cartItemsContainer = document.getElementById("cart-items") //items do carrinho//
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
